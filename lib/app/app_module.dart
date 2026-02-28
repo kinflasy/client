ddd996c88a313df41bd9d17411d3b47fa5ccc6ae
lilicrst-g <lisa.crisilva@outlook.com>
2026-02-27 21:55:44 -0300

    dependências e configuração do modular

---
+++ lib/app/app_module.dart
@@ -0,0 +1,27 @@
+import 'package:flutter_modular/flutter_modular.dart';
+
+class AppModule extends Module {
+  // @override
+  // void binds(Injector i) {
+  //   // TODO: implement binds
+  //   super.binds(i);
+  // }
+
+  // @override
+  // void routes(RouteManager r) {
+  //   // TODO: implement routes
+  //   super.routes(r);
+  // }
+
+  // @override
+  // List<Bind<Object>> get binds => [];
+
+  // @override
+  // List<ModularRoute> get routes => [];
+
+  @override
+  void binds(i) {}
+
+  @override
+  void routes(r) {}
+}

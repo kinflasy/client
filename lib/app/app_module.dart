79633532f825783b0a9693b069dbcd8d844ab2ab
lilicrst-g <lisa.crisilva@outlook.com>
2026-03-03 21:37:41 -0300

    configuração flutter modular

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,27 +1,13 @@
+import 'package:client/app/home_page.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class AppModule extends Module {
-  // @override
-  // void binds(Injector i) {
-  //   // TODO: implement binds
-  //   super.binds(i);
-  // }
-
-  // @override
-  // void routes(RouteManager r) {
-  //   // TODO: implement routes
-  //   super.routes(r);
-  // }
-
-  // @override
-  // List<Bind<Object>> get binds => [];
-
-  // @override
-  // List<ModularRoute> get routes => [];
-
   @override
   void binds(i) {}
 
   @override
-  void routes(r) {}
+  void routes(RouteManager r) {
+    super.routes(r);
+    r.child('/', child: (context) => HomePage(),);
+  }
 }

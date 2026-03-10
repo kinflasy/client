cfb3a29dd285614964ed64c7cc1de332de680190
lilicrst-g <lisa.crisilva@outlook.com>
2026-03-10 11:17:45 -0300

    classe failured com subtipos de erro

---
+++ lib/core/errors/failure.dart
@@ -0,0 +1,24 @@
+sealed class Failure {
+  const Failure(this.message);
+  final String message;
+}
+
+class NetworkFailure extends Failure {
+  const NetworkFailure(super.message);
+}
+
+class ServerFailure extends Failure {
+  const ServerFailure(super.message);
+}
+
+class AuthFailure extends Failure {
+  const AuthFailure(super.message);
+}
+
+class NotFoundFailure extends Failure {
+  const NotFoundFailure(super.message);
+}
+
+class UnknownFailure extends Failure {
+  const UnknownFailure(super.message);
+}
